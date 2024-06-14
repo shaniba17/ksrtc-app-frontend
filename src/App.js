49cd2ba23b000ed6982622da0f9bebfc0916b904
shaniba17 <shanibatm2002@gmail.com>
2024-06-14 08:12:11 +0530

    AddBus added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddBus from './components/AddBus';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div>
+      <AddBus/>
     </div>
   );
 }
